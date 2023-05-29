a5a2a66507c665714d50867727dda317cde74b25
hoanglong21 <longdh21@gmail.com>
2023-05-29 17:55:48 +0700

    Add API StudySet & Card

---
+++ springboot-backend/src/main/java/com/capstone/project/model/Card.java
@@ -0,0 +1,28 @@
+package com.capstone.project.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "card")
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String term;
+
+    private String definition;
+
+    private String note;
+
+    @ManyToOne
+    @JoinColumn(name = "studyset_id", nullable = false)
+    private StudySet studySet;
+}
---
+++ springboot-backend/src/main/java/com/capstone/project/model/StudySet.java
@@ -0,0 +1,31 @@
+package com.capstone.project.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "studyset")
+public class StudySet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+//    private int classId;
+
+    private String title;
+
+    private String description;
+
+    private String status;
+}
