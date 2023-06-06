bfefebfe4a97952ffb49ebb0457a9e4aa16bf51f
tuyethg2001 <tuyethg2792001@gmail.com>
2023-06-06 19:22:54 +0700

    update studyset

--- react-frontend/src/pages/CreateStudySet.jsx
+++ react-frontend/src/pages/CreateStudySet.jsx
@@ -11,32 +11,42 @@ const CreateStudySet = () => {
     return (
         <div className="container mt-4">
             {/* Heading */}
-            <div className="d-flex justify-content-between divider pb-2 px-3">
+            <div className="d-flex justify-content-between divider pb-2 px-3 mb-4">
                 <h3 className="fw-bold">Create a new study set</h3>
-                <button type="button" class="btn btn-primary">
+                <button type="button" className="btn btn-primary">
                     Create
                 </button>
             </div>
             <form className="m-2">
                 {/* Study set */}
-                <div className="form-group mb-3">
-                    <label className={styles.formLabel}>Title</label>
-                    <input
-                        placeholder="Enter your title"
-                        name="title"
-                        className={`form-control ${styles.formControl}`}
-                        value={studySet.title}
-                    />
+                <div className="row">
+                    <div className="form-group mb-3 col-6">
+                        <label className={styles.formLabel}>Title</label>
+                        <input
+                            placeholder="Enter your title"
+                            name="title"
+                            className={`form-control ${styles.formControl}`}
+                            value={studySet.title}
+                        />
+                    </div>
+                    <div className="form-group mb-3 col-6">
+                        <label className={styles.formLabel}>Access</label>
+                        <select
+                            className={`form-select ${styles.formSelect}`}
+                            aria-label="access"
+                        >
+                            <option value="0">Public</option>
+                            <option value="1">Private</option>
+                        </select>
+                    </div>
                 </div>
                 <div className="form-group mb-3">
-                    <label className={styles.formLabel}>Access</label>
-                    <select
-                        class={`form-select ${styles.formSelect}`}
-                        aria-label="access"
-                    >
-                        <option value="0">Public</option>
-                        <option value="1">Private</option>
-                    </select>
+                    <label className={styles.formLabel}>Description</label>
+                    <textarea
+                        class="form-control"
+                        placeholder="Add a description..."
+                        rows="3"
+                    ></textarea>
                 </div>
             </form>
         </div>
