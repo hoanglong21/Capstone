e74f7cdcd253aff6e6758e6f537fdf7c9be2b14c
tuyethg2001 <tuyethg2792001@gmail.com>
2023-06-13 17:28:41 +0700

    feature upload file

--- react-frontend/src/components/Card/Card.jsx
+++ react-frontend/src/components/Card/Card.jsx
@@ -1,48 +1,27 @@
 import { useState } from 'react'
+
+import { uploadFile } from '../../features/uploadFile'
+
 import { DeleteIcon, ImageIcon, MicIcon } from '../../components/icons'
 import TextEditor from '../../components/TextEditor'
 import styles from '../../assets/styles/Card.module.css'
-
-import firebase from 'firebase/compat/app'
-import {
-    getStorage,
-    ref,
-    uploadBytesResumable,
-    getDownloadURL,
-} from 'firebase/storage'
-
-const firebaseConfig = {
-    apiKey: 'AIzaSyD9Fo5y8qhokjfJ_t4Gc0Gd4DXwDC_V2tM',
-    authDomain: 'capstone-project-34253.firebaseapp.com',
-    databaseURL:
-        'https://capstone-project-34253-default-rtdb.asia-southeast1.firebasedatabase.app',
-    projectId: 'capstone-project-34253',
-    storageBucket: 'capstone-project-34253.appspot.com',
-    messagingSenderId: '342570414778',
-    appId: '1:342570414778:web:6f43802265129593d88883',
-    measurementId: 'G-0LG2E3HGPQ',
-}
-
-// Initialize Firebase
-let firebaseApp
-if (!firebase.apps.length) {
-    firebaseApp = firebase.initializeApp(firebaseConfig)
-} else {
-    firebaseApp = firebase.app()
-}
-
-const storage = getStorage(firebaseApp)
 
 export const Card = (props) => {
     const [image, setImage] = useState(null)
     const [previewImage, setPreviewImage] = useState(null)
     const [audio, setAudio] = useState(null)
     const [previewAudio, setPreviewAudio] = useState(null)
+    const [card, setCard] = useState({
+        picture: '',
+        audio: '',
+        studySet: '',
+    })
 
-    const handleChangeImage = (e) => {
-        if (e.target.files[0]) {
-            setImage(e.target.files[0])
-            setPreviewImage(URL.createObjectURL(e.target.files[0]))
+    const handleChangeImage = async (e) => {
+        const file = e.target.files[0]
+        if (file) {
+            setImage(file)
+            setPreviewImage(await uploadFile(file))
         }
     }
 
@@ -61,51 +40,6 @@ export const Card = (props) => {
     const handleDeleteAudio = () => {
         setAudio(null)
         setPreviewAudio(null)
-    }
-
-    const handleUpload = () => {
-        // Create the file metadata
-        /** @type {any} */
-        const metadata = {
-            contentType: 'image/jpeg',
-        }
-
-        const storageRef = ref(storage, 'images/' + image.name)
-
-        const uploadTask = uploadBytesResumable(storageRef, image, metadata)
-
-        // Listen for state changes, errors, and completion of the upload.
-        uploadTask.on(
-            'state_changed',
-            (snapshot) => {
-                // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-                const progress =
-                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-                console.log('Upload is ' + progress + '% done')
-                switch (snapshot.state) {
-                    case 'paused':
-                        console.log('Upload is paused')
-                        break
-                    case 'running':
-                        console.log('Upload is running')
-                        break
-                }
-            },
-            (error) => {
-                // A full list of error codes is available at
-                // https://firebase.google.com/docs/storage/web/handle-errors
-                switch (error.code) {
-                    default:
-                        break
-                }
-            },
-            () => {
-                // Upload completed successfully, now we can get the download URL
-                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                    console.log('File available at', downloadURL)
-                })
-            }
-        )
     }
 
     return (
--- react-frontend/src/index.jsx
+++ react-frontend/src/index.jsx
@@ -6,19 +6,20 @@ import { store } from './state/store'
 import Register from './pages/Register'
 import VideoChatContainer from './components/Chat/VideoChatContainer'
 import ChatContainer from './components/Chat/ChatContainer'
-import StorageContainer from './components/FileManagement/StorageContainer'
+import StorageContainer from './features/StorageContainer'
 import SpeechToText from './components/InputModel/SpeechToText'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import CreateStudySet from './pages/CreateStudySet'
 import Draw from './components/InputModel/Draw'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle'
-import './index.css'
 import GPTContainer from './components/Chat/GPTContainer'
 import TextToSpeech from './components/InputModel/TextToSpeech'
 import Flashcard from './pages/Flashcard'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import './index.css'
 
 function App() {
     return (
@@ -44,7 +45,7 @@ function App() {
                         <Route path="gpt" element={<GPTContainer />} />
                         <Route path="voice" element={<SpeechToText />} />
                         <Route path="draw" element={<Draw />} />
-                        <Route path="tospeech" element={<TextToSpeech/>}/>
+                        <Route path="tospeech" element={<TextToSpeech />} />
                     </Route>
 
                     <Route path="/login" element={<Login />} />
