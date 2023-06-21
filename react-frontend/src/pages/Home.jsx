c60afc134d1b667fb8d77d7da09467829a6e848b
Đỗ Thùy Dương <thuyduong03112001@gmail.com>
2023-06-21 11:33:37 +0700

    add Home
    add Home

--- react-frontend/src/pages/Home.jsx
+++ react-frontend/src/pages/Home.jsx
@@ -1,21 +1,52 @@
-import React from 'react'
+import React from "react";
+import "../assets/styles/Home.css";
+
 
 function Home() {
-    return (
-        <div className="content__search flex-grow-1">
-            <input
-                _ngcontent-rlf-c66=""
-                type="text"
-                alt="enter search"
-                autocomplete="off"
-                id="search-text-box"
-                focus-me=""
-                autofocus=""
-                class="bg-w-gray"
-                placeholder="日本, nihon, Nhật Bản"
-            ></input>
+  return (
+      <><div className="content__search flex-grow-1">
+        <input
+          type="text"
+          alt="enter search"
+          autocomplete="off"
+          id="search-text-box"
+          focus-me=""
+          autofocus=""
+          class="bg-w-gray"
+          placeholder="日本, Japanese, Nhật Bản"
+        ></input>
+      </div>
+      <div
+        id="btn-wrapper"
+        className="btn-group search-option"
+      >
+        <div className="btn-group-wrapper">
+          <button
+            aria-label="word"
+            id="tab0"
+            className="btn btn-tab-search bg-content-df search-option-word tab-active"
+          >
+            {" "}
+            Vocabulary{" "}
+          </button>
+          <button
+            aria-label="kanji"
+            id="tab1"
+            className="btn btn-tab-search bg-content-df search-option-kanji"
+          >
+            Kanji
+          </button>
+          <button
+            aria-label="grammar"
+            id="tab2"
+            className="btn btn-tab-search bg-content-df search-option-grammar"
+          >
+            Grammar
+          </button>
         </div>
-    )
+      </div>
+      </>
+  );
 }
 
-export default Home
+export default Home;
