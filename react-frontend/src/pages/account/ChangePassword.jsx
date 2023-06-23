3554ce52aa9258e5d0e0ef86fd1213910e004ecb
tuyethg2001 <tuyethg2792001@gmail.com>
2023-06-23 16:07:34 +0700

    FE change password

--- react-frontend/src/pages/account/ChangePassword.jsx
+++ react-frontend/src/pages/account/ChangePassword.jsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom'
 import FormStyles from '../../assets/styles/Form.module.css'
 
 const ChangePassword = () => {
     return (
         <div className="mx-5 ps-3 pe-5">
             <h4>Change your password</h4>
-            <form className="mt-4">
+            <form className="mt-5">
                 <div className="form-group mb-3">
                     <label className={FormStyles.formLabel}>
                         Current Password
@@ -39,6 +40,17 @@ const ChangePassword = () => {
                         required
                     />
                 </div>
+                <button className="btn btn-primary px-4 mt-1">Save</button>
+                <p className="mt-3">
+                    If you forgot your password, you can{' '}
+                    <Link
+                        to=""
+                        className="link-primary link-underline-opacity-0 link-underline-opacity-75-hover"
+                    >
+                        Reset your password
+                    </Link>
+                    .
+                </p>
             </form>
         </div>
     )
