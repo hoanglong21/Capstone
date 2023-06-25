a921acb8f8865f207e0490461941607d0a98fa25
Đỗ Thùy Dương <thuyduong03112001@gmail.com>
2023-06-25 21:16:00 +0700

    update to term
    update to term

--- react-frontend/src/components/footer/Term.jsx
+++ react-frontend/src/components/footer/Term.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
 function Term() {
   return (
-    <div>Term</div>
-  )
+    <div>
+      <div className="term__container">
+      <h1 className="term__service">Term Of Service</h1>
+      <p className="term__intro">
+        The ("<strong>NihongoLevelUp</strong>") website and its
+        applications are a hosted service operated by NihongoLevelUp Teams, Inc. ("
+        <strong>Service</strong>"). Any use of the Service is subject to the
+        following Terms and Conditions of Service ("
+        <strong>Terms of Service</strong>" or "<strong>Terms</strong>"), as well
+        as to{" "}
+        <a
+          className=""
+          href="/privacy"
+        >
+          <span>'s Privacy Policy</span>
+        </a>
+        ,{" "} all of which are incorporated by reference into these Terms of
+        Service. Your use of the Service will constitute your acceptance of
+        these Terms of Service.
+      </p>
+      </div>
+    </div>
+  );
 }
 
-export default Term
+export default Term;
