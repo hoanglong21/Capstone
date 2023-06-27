6c0dcc1e1f0953cef274151eb8d66d519b3644da
hoanglong21 <longdh21@gmail.com>
2023-06-27 16:09:30 +0700

    update mail for feedback

--- springboot-backend/src/main/java/com/capstone/project/service/impl/FeedbackServiceImpl.java
+++ springboot-backend/src/main/java/com/capstone/project/service/impl/FeedbackServiceImpl.java
@@ -2,10 +2,17 @@ package com.capstone.project.service.impl;
 
 import com.capstone.project.exception.ResourceNotFroundException;
 import com.capstone.project.model.Feedback;
+import com.capstone.project.model.FeedbackType;
+import com.capstone.project.model.User;
 import com.capstone.project.repository.FeedbackRepository;
+import com.capstone.project.repository.FeedbackTypeRepository;
+import com.capstone.project.repository.UserRepository;
 import com.capstone.project.service.FeedbackService;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,10 +21,17 @@ import java.util.List;
 public class FeedbackServiceImpl implements FeedbackService {
 
     private final FeedbackRepository feedbackRepository;
+    private final UserRepository userRepository;
+    private FeedbackTypeRepository feedbackTypeRepository;
 
     @Autowired
-    public FeedbackServiceImpl(FeedbackRepository feedbackRepository) {
+    private JavaMailSender mailSender;
+
+    @Autowired
+    public FeedbackServiceImpl(FeedbackRepository feedbackRepository, UserRepository userRepository, FeedbackTypeRepository feedbackTypeRepository) {
         this.feedbackRepository = feedbackRepository;
+        this.userRepository = userRepository;
+        this.feedbackTypeRepository = feedbackTypeRepository;
     }
 
     @Override
@@ -27,6 +41,7 @@ public class FeedbackServiceImpl implements FeedbackService {
 
     @Override
     public Feedback createFeedback(Feedback feedback) {
+        sendFeedbackEmail(feedback);
         return feedbackRepository.save(feedback);
     }
 
@@ -54,5 +69,58 @@ public class FeedbackServiceImpl implements FeedbackService {
                 .orElseThrow(() -> new ResourceNotFroundException("Feedback not exist with id: " + id));
         feedbackRepository.delete(feedback);
         return true;
+    }
+
+    private void sendFeedbackEmail(Feedback feedback) {
+        try {
+            User user = userRepository.findUserById(feedback.getUser().getId());
+            if (user == null) {
+                throw new ResourceNotFroundException("User not found with id: " + feedback.getUser().getId());
+            }
+            FeedbackType feedbackType = feedbackTypeRepository.findById(feedback.getFeedbackType().getId())
+                    .orElseThrow(() -> new ResourceNotFroundException("Feedback Type not found with id: " + feedback.getFeedbackType().getId()));
+
+            String toAddress = user.getEmail();
+            String fromAddress = "nihongolevelup.box@gmail.com";
+            String senderName = "NihongoLevelUp";
+
+            String subject = "Thank You for Your Feedback!";
+            String content = "";
+            if(feedback.getDestination().contains("/")) {
+                content = "Dear [[name]],<br><br>"
+                        + "Thank you for taking the time to provide us with your feedback. We really appreciate it!<br><br>"
+                        + "We will carefully review your comments and suggestions regarding " + feedbackType.getName() + " to see how we can improve our services.<br><br>"
+                        + "Here are the details of your feedback:<br><br>"
+                        + "Title: " + feedback.getTitle() + "<br>"
+                        + "Type: " + feedbackType.getName() + "<br>"
+                        + "Content: " + feedback.getContent() + "<br>"
+                        + "To: " + feedback.getDestination() + "<br><br>"
+                        + "If you have any additional feedback or questions, please do not hesitate to contact us at nihongolevelup.box@gmail.com.<br><br>"
+                        + "Best regards,<br>"
+                        + "NihongoLevelUp Team";
+            } else {
+                content = "Dear [[name]],<br><br>"
+                        + "Thank you for taking the time to provide us with your feedback. We really appreciate it!<br><br>"
+                        + "We will carefully review your comments and suggestions to see how we can improve our services.<br><br>"
+                        + "If you have any additional feedback or questions, please do not hesitate to contact us at nihongolevelup.box@gmail.com.<br><br>"
+                        + "Best regards,<br>"
+                        + "NihongoLevelUp Team";
+            }
+
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message);
+
+            helper.setFrom(fromAddress, senderName);
+            helper.setTo(toAddress);
+            helper.setSubject(subject);
+
+            content = content.replace("[[name]]", user.getFirst_name() + " " + user.getLast_name());
+
+            helper.setText(content, true);
+
+            mailSender.send(message);
+        } catch (Exception e) {
+
+        }
     }
 }
