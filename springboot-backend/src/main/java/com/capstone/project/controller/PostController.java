d580f1a67b64264084c708b3d3716b7038b18514
Quantruong355 <97685567+Quantruong355@users.noreply.github.com>
2023-06-29 01:41:51 +0700

    Add validation

--- springboot-backend/src/main/java/com/capstone/project/controller/PostController.java
+++ springboot-backend/src/main/java/com/capstone/project/controller/PostController.java
@@ -1,11 +1,16 @@
 package com.capstone.project.controller;
 
+import com.capstone.project.dto.PostRequest;
 import com.capstone.project.exception.ResourceNotFroundException;
 import com.capstone.project.model.Class;
 import com.capstone.project.model.Post;
 import com.capstone.project.service.PostService;
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.DateFormat;
@@ -15,10 +20,15 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 @CrossOrigin
 @RestController
 @RequestMapping("/api/v1/")
 public class PostController {
+
+    @Autowired
+    private ModelMapper modelMapper;
 
     private final PostService postService;
 @Autowired
@@ -38,8 +48,22 @@ public class PostController {
     }
 
     @PostMapping("/post")
-    public ResponseEntity<?> createPost(@RequestBody Post post)  {
-        return ResponseEntity.ok(postService.createPost(post));
+    public ResponseEntity<?> createPost(@Valid @RequestBody PostRequest postRequest, BindingResult result)  {
+        if (result.hasErrors()) {
+            // create a list of error messages from the binding result
+            List<String> errors = result.getAllErrors().stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .collect(Collectors.toList());
+            return ResponseEntity.badRequest().body(errors);
+        } else {
+            Post post = modelMapper.map(postRequest,Post.class);
+            try{
+                return ResponseEntity.ok(postService.createPost(post));
+            } catch (Exception e) {
+                return ResponseEntity.badRequest().body(e.getMessage());
+            }
+        }
+
     }
 
     @GetMapping("/post/{id}")
@@ -52,12 +76,22 @@ public class PostController {
     }
 
     @PutMapping("/post/{id}")
-    public ResponseEntity<?> updatePost(@RequestBody Post posts, @PathVariable int id) {
-    try {
-        return ResponseEntity.ok(postService.updatePost(posts, id));
-    } catch (ResourceNotFroundException e){
-        return ResponseEntity.badRequest().body(e.getMessage());
-    }
+    public ResponseEntity<?> updatePost(@Valid @RequestBody PostRequest postRequest, @PathVariable int id,BindingResult result ) {
+        if (result.hasErrors()) {
+            // create a list of error messages from the binding result
+            List<String> errors = result.getAllErrors().stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .collect(Collectors.toList());
+            return ResponseEntity.badRequest().body(errors);
+        } else {
+            Post post = modelMapper.map(postRequest,Post.class);
+            try {
+                return ResponseEntity.ok(postService.updatePost(post, id));
+            } catch (ResourceNotFroundException e){
+                return ResponseEntity.badRequest().body(e.getMessage());
+            }
+        }
+
     }
 
     @DeleteMapping("/post/{id}")
--- springboot-backend/src/main/java/com/capstone/project/controller/SubmissionController.java
+++ springboot-backend/src/main/java/com/capstone/project/controller/SubmissionController.java
@@ -1,21 +1,31 @@
 package com.capstone.project.controller;
 
+import com.capstone.project.dto.SubmissionRequest;
 import com.capstone.project.exception.ResourceNotFroundException;
 import com.capstone.project.model.Class;
 import com.capstone.project.model.Submission;
 import com.capstone.project.service.SubmissionService;
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 @CrossOrigin
 @RestController
 @RequestMapping("/api/v1/")
 public class SubmissionController {
+
+    @Autowired
+    private ModelMapper modelMapper;
 
     private final SubmissionService submissionService;
 @Autowired
@@ -34,11 +44,23 @@ public class SubmissionController {
     }
 
     @PostMapping("/submissions")
-    public ResponseEntity<?> createSubmission(@RequestBody Submission submission) throws ParseException {
-        return ResponseEntity.ok(submissionService.createSubmission(submission));
+    public ResponseEntity<?> createSubmission(@Valid @RequestBody SubmissionRequest submissionRequest, BindingResult result) {
+        if (result.hasErrors()) {
+            // create a list of error messages from the binding result
+            List<String> errors = result.getAllErrors().stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .collect(Collectors.toList());
+            return ResponseEntity.badRequest().body(errors);
+        } else {
+            Submission submission = modelMapper.map(submissionRequest,Submission.class);
+            try{
+                return ResponseEntity.ok(submissionService.createSubmission(submission));
+            } catch (Exception e) {
+                return ResponseEntity.badRequest().body(e.getMessage());
+            }
+        }
+
     }
-
-
 
     @GetMapping("/submissions/{id}")
     public ResponseEntity<?> getSubmissionById(@PathVariable int id) {
@@ -49,13 +71,23 @@ public class SubmissionController {
     }
     }
 
+
     @PutMapping("/submissions/{id}")
-    public ResponseEntity<?> updateSubmission(@RequestBody Submission submission, @PathVariable int id) {
-    try {
-        return ResponseEntity.ok(submissionService.updateSubmission(id, submission));
-    } catch (ResourceNotFroundException e){
-        return ResponseEntity.badRequest().body(e.getMessage());
-    }
+    public ResponseEntity<?> updateSubmission(@Valid @RequestBody SubmissionRequest submissionRequest, @PathVariable int id,BindingResult result) {
+        if (result.hasErrors()) {
+            // create a list of error messages from the binding result
+            List<String> errors = result.getAllErrors().stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .collect(Collectors.toList());
+            return ResponseEntity.badRequest().body(errors);
+        } else {
+            Submission submission = modelMapper.map(submissionRequest,Submission.class);
+            try {
+                return ResponseEntity.ok(submissionService.updateSubmission(id, submission));
+            } catch (ResourceNotFroundException e){
+                return ResponseEntity.badRequest().body(e.getMessage());
+            }
+        }
     }
 
     @DeleteMapping("/submissions/{id}")
