f838262c822827e05b88b08d739e1b48798c831e
Đỗ Thùy Dương <thuyduong03112001@gmail.com>
2023-06-30 22:01:21 +0700

    fix a bit
    fix a bit

--- react-frontend/src/pages/Dictionary.jsx
+++ react-frontend/src/pages/Dictionary.jsx
@@ -23,22 +23,22 @@ function Dictionary() {
   return (
     <div className="dictionary_header">
       <div className="container mx-auto py-8">
-        <h2 className="text-3xl font-bold text-center text-black ">
+        <h2 className="text-3xl font-bold text-center text-black mt-4">
           Nihongo Level Up Dictionary
         </h2>
         <p className="text-center mt-1 mb-10 text-slate-300 text-lg">
           Find definition for word
         </p>
 
-        <div className="flex items-center justify-center mt-5">
-          <div className="flex border-2 border-gray-200 rounded">
+        <div className="flex items-center justify-center mt-4">
+          <div className="flex border-2 text-center">
             <input
-              className="px-4 py-2 md:w-100"
+              className="px-4 py-2 md:w-100 rounded-2xl"
               type="text"
               placeholder="Search..."
             />
             <button
-              className="bg-blue-400 border-l px-4 py-2 text-black"
+              className="bg-blue-400 border-l px-4 py-2 text-black rounded-2xl"
             >
               Search
             </button>
