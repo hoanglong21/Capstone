6e4dd7eb445f61a93646e22b6cb21ea947533505
Đỗ Thùy Dương <thuyduong03112001@gmail.com>
2023-06-30 22:53:49 +0700

    fix css
    fix css

--- react-frontend/src/pages/Dictionary.jsx
+++ react-frontend/src/pages/Dictionary.jsx
@@ -1,47 +1,47 @@
 import React from "react";
 import { useContext, useState, createContext } from "react";
 
-
 export const InputContext = createContext();
 function Dictionary() {
-//   const [value, setValue] = useState("");
-//   const { inputValue, setInputValue } = useContext(InputContext);
+  //   const [value, setValue] = useState("");
+  //   const { inputValue, setInputValue } = useContext(InputContext);
 
-//   const handleInputChange = e => setValue(e.target.value);
+  //   const handleInputChange = e => setValue(e.target.value);
 
-//   const handleSubmit = () => {
-//     setInputValue(value);
-//     setValue("");
-//   }
+  //   const handleSubmit = () => {
+  //     setInputValue(value);
+  //     setValue("");
+  //   }
 
-//   const handleInputKeyDown = (e) => {
-//     if(e.key === 'Enter') {
-//       setInputValue(value);
-//       setValue("")
-//     }
-//   }
+  //   const handleInputKeyDown = (e) => {
+  //     if(e.key === 'Enter') {
+  //       setInputValue(value);
+  //       setValue("")
+  //     }
+  //   }
   return (
-    <div className="dictionary_header">
+    <div className="dictionary_header bg-light">
       <div className="container mx-auto py-8">
-        <h2 className="text-3xl font-bold text-center text-black mt-4">
+        <h3 className="text-3xl font-bold text-center text-black mt-3">
           Nihongo Level Up Dictionary
-        </h2>
+        </h3>
         <p className="text-center mt-1 mb-10 text-slate-300 text-lg">
           Find definition for word
         </p>
 
-        <div className="flex items-center justify-center mt-4">
-          <div className="flex border-2 text-center">
-            <input
-              className="px-4 py-2 md:w-100 rounded-2xl"
-              type="text"
-              placeholder="Search..."
-            />
-            <button
-              className="bg-blue-400 border-l px-4 py-2 text-black rounded-2xl"
-            >
-              Search
-            </button>
+        <div className="justify-center mt-4">
+          <div className="text-center form-inline">
+            <form class="form-inline w-50" style={{display: "inline-flex"}}>
+              <input
+                class="form-control mr-sm-2"
+                type="search"
+                placeholder="Search..."
+                aria-label="Search"
+              />
+              <button class="btn btn-primary my-2 my-sm-0" type="submit" style={{marginLeft: "1rem"}}>
+                Search
+              </button>
+            </form>
           </div>
         </div>
 
@@ -53,6 +53,7 @@ function Dictionary() {
         )} */}
       </div>
     </div>
+    
   );
 }
 
