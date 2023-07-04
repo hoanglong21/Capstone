e06fa75eaf5fb4a5235f02bcefbb72e6607754eb
hoanglong21 <longdh21@gmail.com>
2023-07-04 11:38:25 +0700

    Add 3 Translates

--- springboot-backend/src/main/java/com/capstone/project/controller/TranslateController.java
+++ springboot-backend/src/main/java/com/capstone/project/controller/TranslateController.java
@@ -7,15 +7,33 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1/translate/")
 public class TranslateController {
     @Autowired
     private TranslateService translateService;
 
-    @GetMapping("/translate")
-    public ResponseEntity<?> translate(@RequestParam("text") String text, @RequestParam("to") String to) {
+    @GetMapping("clients5")
+    public ResponseEntity<?> translateClients5(@RequestParam("text") String text, @RequestParam("to") String to) {
         try {
-            return ResponseEntity.ok(translateService.translate(text, to));
+            return ResponseEntity.ok(translateService.translateClients5(text, to));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("googleapis")
+    public ResponseEntity<?> translateGoogleapis(@RequestParam("text") String text, @RequestParam("to") String to) {
+        try {
+            return ResponseEntity.ok(translateService.translateGoogleapis(text, to));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("mymemory")
+    public ResponseEntity<?> translateMymemory(@RequestParam("text") String text, @RequestParam("to") String to) {
+        try {
+            return ResponseEntity.ok(translateService.translateMymemory(text, to));
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
