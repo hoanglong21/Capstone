8a3ab4e368b1107634e318f4420dd65b68f7854f
tuyethg2001 <tuyethg2792001@gmail.com>
2023-07-05 18:30:16 +0700

    FE translate
    switch language, translate, analysis

--- react-frontend/src/components/InputModel/SpeechToText.jsx
+++ react-frontend/src/components/InputModel/SpeechToText.jsx
@@ -1,34 +1,36 @@
-import React from 'react';
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import SpeechRecognition, {
+    useSpeechRecognition,
+} from 'react-speech-recognition'
 
 const SpeechToText = () => {
-  const {
-    transcript,
-    listening,
-    resetTranscript,
-    browserSupportsSpeechRecognition
-  } = useSpeechRecognition({
-    // Set language code to Japanese
-    language: 'ja',
-    // Display final result only
-    interimResults: false
-  });
+    const {
+        transcript,
+        listening,
+        resetTranscript,
+        browserSupportsSpeechRecognition,
+    } = useSpeechRecognition({
+        // Set language code to Japanese
+        language: 'ja',
+        // Display final result only
+        interimResults: false,
+    })
 
-  const startListening = () => SpeechRecognition.startListening({ continuous: true, language: 'ja' });
+    const startListening = () =>
+        SpeechRecognition.startListening({ continuous: true, language: 'ja' })
 
-  if (!browserSupportsSpeechRecognition) {
-    return <span>Browser doesn't support speech recognition.</span>;
-  }
+    if (!browserSupportsSpeechRecognition) {
+        return <span>Browser doesn't support speech recognition.</span>
+    }
 
-  return (
-    <div>
-      <p>Microphone: {listening ? 'on' : 'off'}</p>
-      <button onClick={startListening}>Start</button>
-      <button onClick={SpeechRecognition.stopListening}>Stop</button>
-      <button onClick={resetTranscript}>Reset</button>
-      <p>{transcript}</p>
-    </div>
-  );
-};
+    return (
+        <div>
+            <p>Microphone: {listening ? 'on' : 'off'}</p>
+            <button onClick={startListening}>Start</button>
+            <button onClick={SpeechRecognition.stopListening}>Stop</button>
+            <button onClick={resetTranscript}>Reset</button>
+            <p>{transcript}</p>
+        </div>
+    )
+}
 
-export default SpeechToText;
+export default SpeechToText
--- react-frontend/src/pages/translate/Translate.jsx
+++ react-frontend/src/pages/translate/Translate.jsx
@@ -1,55 +1,221 @@
 import { useState } from 'react'
 
-import { ExchangeIcon } from '../../components/icons'
+import TranslateService from '../../services/TranslateService'
+import DetectionService from '../../services/DetectionService'
+
+import { ExchangeIcon, TranslateIcon } from '../../components/icons'
 import './Translate.css'
+import { useEffect } from 'react'
 
 function Translate() {
     const [origText, setOrigText] = useState('')
+    const [origLang, setOrigLang] = useState('ja')
     const [transText, setTransText] = useState('')
-    const [sourceLang, setSourceLang] = useState('id')
-    const [targetLang, setTargetLang] = useState('en')
+    const [transLang, setTransLang] = useState('vi')
+    const [loadingTrans, setLoadingTrans] = useState(false)
+
+    const [isSwitch, setIsSwitch] = useState(false)
+
+    const [grammarCheck, setGrammarCheck] = useState('')
+    const [loadingCheck, setLoadingCheck] = useState(false)
+
+    const [analysis, setAnalysis] = useState([])
+    const [loadingAnalysis, setLoadingAnalysis] = useState(false)
+
+    const [openAI, setOpenAI] = useState('')
+    const [loadingOpenAI, setLoadingOpenAI] = useState(false)
+
     const [error, setError] = useState('')
 
-    async function switchLanguage() {
-        const oldTargetLang = targetLang
-        const oldTransText = transText
-        setTargetLang(sourceLang)
-        setSourceLang(oldTargetLang)
-        setTransText(origText)
-        setOrigText(oldTransText)
+    useEffect(() => {
+        if (origText.length > 5000) {
+            setError('Your data exceeds the allowed length.')
+        } else {
+            setError('')
+        }
+    }, [origText])
+
+    const translate = async () => {
+        setGrammarCheck('')
+        setAnalysis([])
+        setLoadingOpenAI('')
+        if (origText.length > 0 && origText.length <= 5000) {
+            // translate
+            setLoadingTrans(true)
+            setLoadingCheck(true)
+            setLoadingAnalysis(true)
+            setLoadingOpenAI(true)
+            let resTrans = ''
+            try {
+                resTrans = (
+                    await TranslateService.translateClients5(
+                        origText,
+                        transLang
+                    )
+                ).data
+            } catch (error) {
+                console.log(error)
+            }
+            if (!resTrans) {
+                try {
+                    resTrans = (
+                        await TranslateService.translateGoogleapis(
+                            origText,
+                            transLang
+                        )
+                    ).data
+                } catch (error) {
+                    setLoadingTrans(false)
+                    if (error.response && error.response.data) {
+                        setError(error.response.data)
+                    } else {
+                        setError(error.message)
+                    }
+                    return
+                }
+            }
+            setTransText(resTrans)
+            setLoadingTrans(false)
+            // grammar check
+            if (origLang === 'ja') {
+                try {
+                    const resGrammarCheck = (
+                        await DetectionService.grammarCheck(origText)
+                    ).data
+                    setGrammarCheck(resGrammarCheck)
+                } catch (error) {
+                    setLoadingAnalysis(false)
+                    if (error.response && error.response.data) {
+                        setGrammarCheck(error.response.data)
+                    } else {
+                        setGrammarCheck(error.message)
+                    }
+                    return
+                }
+            }
+            setLoadingCheck(false)
+            // analysis
+            try {
+                if (origLang === 'ja') {
+                    const resOrigAnalysis = (
+                        await DetectionService.detectVocabulary(origText)
+                    ).data
+                    setAnalysis(resOrigAnalysis)
+                }
+                if (transLang === 'ja') {
+                    const resTransAnalysis = (
+                        await DetectionService.detectVocabulary(resTrans)
+                    ).data
+                    setAnalysis(resTransAnalysis)
+                }
+            } catch (error) {
+                setLoadingAnalysis(false)
+                if (error.response && error.response.data) {
+                    setAnalysis(error.response.data)
+                } else {
+                    setAnalysis(error.message)
+                }
+                return
+            }
+            setLoadingAnalysis(false)
+            // openAI
+            try {
+                if (origLang === 'ja') {
+                    const resOrigOpenAI = (
+                        await DetectionService.detectGrammar(origText, 'en')
+                    ).data
+                    setOpenAI(resOrigOpenAI)
+                }
+                if (transLang === 'ja') {
+                    const resTransOpenAI = (
+                        await DetectionService.detectGrammar(resTrans, 'en')
+                    ).data
+                    setOpenAI(resTransOpenAI)
+                }
+            } catch (error) {
+                setLoadingOpenAI(false)
+                if (error.response && error.response.data) {
+                    setOpenAI(error.response.data)
+                } else {
+                    setOpenAI(error.message)
+                }
+                return
+            }
+            setLoadingOpenAI(false)
+        }
     }
 
+    useEffect(() => {
+        if (isSwitch) {
+            translate()
+            setIsSwitch(false)
+        }
+    }, [isSwitch])
+
     const autoGrow = (event) => {
-        console.log(event.target.style.height)
         event.target.style.height = '5px'
         event.target.style.height = event.target.scrollHeight + 'px'
     }
 
+    const switchLanguage = () => {
+        setOrigLang(transLang)
+        setTransLang(origLang)
+        setOrigText(transText)
+        setTransText('')
+        setIsSwitch(true)
+    }
+
     return (
-        <div className="translateContainer mx-auto mt-4">
-            <div class="card">
-                <div class="card-header d-flex">
+        <div className="translateContainer mx-auto mt-4 mb-5">
+            {/* error message */}
+            {error && (
+                <div className="alert alert-danger" role="alert">
+                    {error}
+                </div>
+            )}
+            {/* Translate */}
+            <div className="card">
+                <div className="card-header d-flex justify-content-center">
                     <div className="flex-fill d-flex justify-content-center">
                         <select
-                            id="sourceLang"
-                            name="sourceLang"
-                            class="form-select selectLang"
-                            value={sourceLang}
+                            id="targetLang"
+                            name="targetLang"
+                            className="form-select selectLang"
+                            value={origLang}
+                            onChange={(event) => {
+                                const temp = event.target.value
+                                setOrigLang(temp)
+                                if (temp === transLang) {
+                                    setTransLang(origLang)
+                                }
+                            }}
                         >
                             <option value="ja">Japan</option>
                             <option value="vi">Vietnamese</option>
                             <option value="en">English</option>
                         </select>
                     </div>
-                    <button className="btn mx-3" onClick={switchLanguage}>
-                        <ExchangeIcon size="1.375rem" />
+                    <button
+                        className="btn mx-3"
+                        onClick={() => {
+                            switchLanguage()
+                        }}
+                    >
+                        <ExchangeIcon size="1.375rem" />{' '}
                     </button>
                     <div className="flex-fill d-flex justify-content-center">
                         <select
                             id="targetLang"
                             name="targetLang"
-                            class="form-select selectLang"
-                            value={targetLang}
+                            className="form-select selectLang"
+                            value={transLang}
+                            onChange={(event) => {
+                                const temp = event.target.value
+                                setTransLang(temp)
+                                if (temp === origLang) {
+                                    setOrigLang(transLang)
+                                }
+                            }}
                         >
                             <option value="ja">Japan</option>
                             <option value="vi">Vietnamese</option>
@@ -57,21 +223,195 @@ function Translate() {
                         </select>
                     </div>
                 </div>
-                <div class="card-body d-flex p-0">
+                <div className="card-body d-flex p-0">
                     <textarea
-                        class="form-control translateInput"
+                        className="form-control translateInput"
                         id="floatingTextarea"
+                        value={origText}
                         onInput={autoGrow}
+                        onChange={(event) => {
+                            setOrigText(event.target.value)
+                        }}
                     ></textarea>
                     <textarea
-                        class="form-control translateInput"
+                        className="form-control translateInput"
                         id="floatingTextarea"
+                        value={transText}
                         onInput={autoGrow}
                         disabled
                     ></textarea>
                 </div>
-                <div class="card-footer text-body-secondary text-end">
+                <div className="card-footer d-flex justify-content-between">
+                    <button
+                        id="translateButton"
+                        className="btn btn-primary btn-sm d-flex align-items-center justify-content-center"
+                        style={{ minWidth: '7rem', minHeight: '2.3rem' }}
+                        onClick={translate}
+                        disabled={loadingTrans}
+                    >
+                        {loadingTrans ? (
+                            <div>
+                                <span
+                                    className="spinner-border spinner-border-sm"
+                                    role="status"
+                                    aria-hidden="true"
+                                ></span>
+                                <span className="visually-hidden">
+                                    Loading...
+                                </span>
+                            </div>
+                        ) : (
+                            <div className="d-flex align-items-center">
+                                <TranslateIcon
+                                    size="1.1rem"
+                                    strokeWidth="1.75"
+                                />
+                                <span className="ms-1">Translate</span>
+                            </div>
+                        )}
+                    </button>
                     <div>{origText.length} / 5000</div>
+                </div>
+            </div>
+            <div className="accordion mt-4" id="accordion">
+                {/* Grammar check */}
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button
+                            className="accordion-button collapsed"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#grammarCheck"
+                            aria-expanded="false"
+                            aria-controls="grammarCheck"
+                        >
+                            Grammar Check
+                        </button>
+                    </h2>
+                    <div
+                        id="grammarCheck"
+                        className="accordion-collapse collapse"
+                    >
+                        <div className="accordion-body">
+                            {loadingCheck ? (
+                                <div className="text-center">
+                                    <div
+                                        className="spinner-border"
+                                        role="status"
+                                    >
+                                        <span className="visually-hidden">
+                                            Loading...
+                                        </span>
+                                    </div>
+                                </div>
+                            ) : (
+                                grammarCheck
+                            )}
+                        </div>
+                    </div>
+                </div>
+                {/* Analysis */}
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button
+                            className="accordion-button collapsed"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#analysis"
+                            aria-expanded="false"
+                            aria-controls="analysis"
+                        >
+                            Analysis
+                        </button>
+                    </h2>
+                    <div id="analysis" className="accordion-collapse collapse">
+                        <div className="accordion-body">
+                            {loadingAnalysis ? (
+                                <div className="text-center">
+                                    <div
+                                        className="spinner-border"
+                                        role="status"
+                                    >
+                                        <span className="visually-hidden">
+                                            Loading...
+                                        </span>
+                                    </div>
+                                </div>
+                            ) : Array.isArray(analysis) ? (
+                                analysis.length === 0 ? (
+                                    ''
+                                ) : (
+                                    <table className="table table-responsive table-hover">
+                                        <thead className="table-light">
+                                            <tr>
+                                                <th scope="col">#</th>
+                                                <th scope="col">Word</th>
+                                                <th scope="col">
+                                                    Part of speech
+                                                </th>
+                                                <th scope="col">
+                                                    Dictionary form
+                                                </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {analysis.map((word, index) => (
+                                                <tr key={index}>
+                                                    <th scope="row">
+                                                        {index + 1}
+                                                    </th>
+                                                    <td>{word.word}</td>
+                                                    <td>{word.partOfSpeech}</td>
+                                                    <td>
+                                                        {word.dictionaryForm}
+                                                    </td>
+                                                </tr>
+                                            ))}
+                                        </tbody>
+                                    </table>
+                                )
+                            ) : (
+                                analysis
+                            )}
+                        </div>
+                    </div>
+                </div>
+                {/* OpenAI */}
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button
+                            className="accordion-button collapsed"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#openAI"
+                            aria-expanded="false"
+                            aria-controls="openAI"
+                        >
+                            OpenAI Integration
+                        </button>
+                    </h2>
+                    <div
+                        id="openAI"
+                        className="accordion-collapse collapse"
+                        style={{ whiteSpace: 'pre-wrap' }}
+                    >
+                        <div className="accordion-body">
+                            {loadingOpenAI ? (
+                                <div className="text-center">
+                                    <div
+                                        className="spinner-border"
+                                        role="status"
+                                    >
+                                        <span className="visually-hidden">
+                                            Loading...
+                                        </span>
+                                    </div>
+                                </div>
+                            ) : (
+                                openAI
+                            )}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
