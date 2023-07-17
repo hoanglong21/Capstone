aa9028a0e1cc69cee5f94dcabccc7c5a0496abc1
Đỗ Thùy Dương <thuyduong03112001@gmail.com>
2023-07-17 22:06:08 +0700

    update

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -70,7 +70,6 @@ import AuthVerify from './components/common/authVerify'
 import AuthService from './services/AuthService'
 import People from './pages/class/People'
 import Mark from './pages/class/Mark'
-import ABC from './pages/admin/ABC'
 const App = () => {
     const { userToken } = useSelector((state) => state.auth)
 
@@ -240,7 +239,6 @@ const App = () => {
                 {/* Quiz */}
                 <Route path="/doquiz" element={<DoQuiz />} />
                 <Route path="/quizanswer" element={<QuizAnswer />} />
-                <Route path="/hihihi" element={<ABC />} />
             </Routes>
             <AuthVerify logOut={logOut} />
         </BrowserRouter>
