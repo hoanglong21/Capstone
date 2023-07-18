dbb4f36262dfc7a0603fdd32203a2ba71bb46231
Đỗ Thùy Dương <thuyduong03112001@gmail.com>
2023-07-19 01:56:21 +0700

    add

--- react-frontend/src/pages/admin/BanUser.jsx
+++ react-frontend/src/pages/admin/BanUser.jsx
@@ -1,6 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import UserService from "../../services/UserService";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from 'react-redux'
 
 function BanUser() {
+  let navigate = useNavigate();
+  const { userInfo } = useSelector((state) => state.user)
+  const [banUser, setBanUser] = useState({});
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (banUser.username) {
+      setBanUser({ ...banUser });
+    }
+  }, [banUser]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    setLoading(true);
+    try {
+      await UserService.banUser(banUser.username);
+      document.getElementById("banUserModal").click();
+      navigate("/");
+      setError("");
+    } catch (error) {
+      if (error.response && error.response.data) {
+        setError(error.response.data);
+      } else {
+        setError(error.message);
+      }
+    }
+    setLoading(false);
+  };
   return (
     <div class="modal" tabindex="-1" role="dialog" id="banModal">
       <div class="modal-dialog" role="document">
@@ -13,14 +46,19 @@ function BanUser() {
               className="btn-close"
               data-bs-dismiss="modal"
               aria-label="Close"
-            >
-            </button>
+            ></button>
           </div>
           <div class="modal-body">
-            <p>Are you sure ban user id ?</p>
+            <p>Are you sure ban {banUser.username} ?</p>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-danger">
+            <button
+            id="banUserModal"
+              type="button"
+              class="btn btn-danger"
+              onClick={handleSubmit}
+              disabled={loading}
+            >
               Sure!
             </button>
             <button
