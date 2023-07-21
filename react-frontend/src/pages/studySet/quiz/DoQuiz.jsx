1a8e63063671e23e3fe97cf23c5eb004418c695b
tuyethg2001 <tuyethg2792001@gmail.com>
2023-07-21 23:17:45 +0700

    FE do quiz

--- react-frontend/src/pages/studySet/quiz/DoQuiz.jsx
+++ react-frontend/src/pages/studySet/quiz/DoQuiz.jsx
@@ -5,6 +5,7 @@ import {
     LearnSolidIcon,
     TestSolidIcon,
 } from '../../../components/icons'
+import FormStyles from '../../../assets/styles/Form.module.css'
 import './quiz.css'
 
 function DoQuiz() {
@@ -402,7 +403,7 @@ d) Tri thức"
             </div>
             {/* Option modal */}
             <div
-                class="modal fade"
+                class="modal fade quizOptionModal"
                 id="quizOptionModal"
                 tabindex="-1"
                 aria-labelledby="quizOptionModalLabel"
@@ -411,11 +412,9 @@ d) Tri thức"
                 <div class="modal-dialog modal-lg">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h4 class="modal-title" id="quizOptionModalLabel">
-                                <strong>MLN111</strong>
-                                <br />
-                                Set up your quiz
-                            </h4>
+                            <h3 class="modal-title" id="quizOptionModalLabel">
+                                Options
+                            </h3>
                             <button
                                 type="button"
                                 class="btn-close"
@@ -424,141 +423,124 @@ d) Tri thức"
                             ></button>
                         </div>
                         <div class="modal-body">
-                            <form action="">
-                                <div
-                                    class="ojn81eb"
-                                    style={{
-                                        display: 'flex',
-                                        justifyContent: 'space-between',
-                                        margin: '1rem 0',
-                                    }}
-                                >
-                                    <div
-                                        class="qa7hpt4"
-                                        style={{
-                                            display: 'flex',
-                                            justifyContent: 'flex-start',
-                                        }}
-                                    >
-                                        <h5
-                                            class="qh8uqkb"
-                                            style={{ marginRight: '.5rem' }}
-                                        >
-                                            <strong>Questions</strong>
-                                        </h5>
-                                        <div class="qxkjrwb">(max 408)</div>
-                                    </div>
-                                    <div
-                                        class="qasqj0a"
-                                        style={{ width: '5.375rem' }}
-                                    >
-                                        <label class="AssemblyInput">
+                            <button className="quizOption_newBtn btn btn-primary">
+                                Create new quiz
+                            </button>
+                            <div className="row mt-4">
+                                <div className="col-6">
+                                    <div className="quizOptionBlock mb-4">
+                                        <legend>QUESTION TYPES</legend>
+                                        <div className="mb-2">
                                             <input
-                                                name="questionCount"
-                                                aria-label="Enter the desired number of questions"
-                                                class="AssemblyInput-input"
-                                                placeholder=""
-                                                type="number"
-                                                value="1"
+                                                className={`form-check-input ${FormStyles.formCheckInput} ms-0`}
+                                                type="checkbox"
+                                                value=""
+                                                id="written"
                                             />
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="ojn81eb">
-                                    <div
-                                        class="qa7hpt4"
-                                        style={{
-                                            display: 'flex',
-                                            justifyContent: 'flex-start',
-                                        }}
-                                    >
-                                        <h5
-                                            class="qh8uqkb"
-                                            style={{ marginRight: '.5rem' }}
-                                        >
-                                            <strong>Answer with</strong>
-                                        </h5>
-                                    </div>
-                                    <div class="select">
-                                        <select
-                                            class="form-select"
-                                            aria-label="Default select example"
-                                        >
-                                            <option selected>Term</option>
-                                            <option value="1">
-                                                Definition
-                                            </option>
-                                            <option value="2">Both</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <hr class="d1llavan" />
-                                <div>
-                                    <div class="ojn81eb">
-                                        <div class="o5jd1da">
-                                            <h5
-                                                class="qh8uqkb"
-                                                style={{ marginRight: '.5rem' }}
+                                            <label
+                                                className="form-check-label"
+                                                for="written"
                                             >
-                                                <strong>True/False</strong>
-                                            </h5>
+                                                Written
+                                            </label>
                                         </div>
-                                        <div class="form-check form-switch">
+                                        <div className="mb-2">
                                             <input
-                                                class="form-check-input"
+                                                className={`form-check-input ${FormStyles.formCheckInput} ms-0`}
                                                 type="checkbox"
-                                                role="switch"
-                                                id="flexSwitchCheckChecked"
-                                                checked
+                                                value=""
+                                                id="mupltipleChoice"
                                             />
+                                            <label
+                                                className="form-check-label"
+                                                for="mupltipleChoice"
+                                            >
+                                                Multiple choice
+                                            </label>
+                                        </div>
+                                        <div>
+                                            <input
+                                                className={`form-check-input ${FormStyles.formCheckInput} ms-0`}
+                                                type="checkbox"
+                                                value=""
+                                                id="trueFalse"
+                                            />
+                                            <label
+                                                className="form-check-label"
+                                                for="trueFalse"
+                                            >
+                                                True/False
+                                            </label>
                                         </div>
                                     </div>
-                                    <div class="ojn81eb">
-                                        <h5
-                                            class="qh8uqkb"
-                                            style={{ marginRight: '.5rem' }}
-                                        >
-                                            <strong>Multiple Choice</strong>
-                                        </h5>
-                                        <div class="form-check form-switch">
+                                    <div className="quizOptionBlock mb-4">
+                                        <legend>QUESTION LIMIT</legend>
+                                        <div className="mb-2 d-flex align-items-center">
                                             <input
-                                                class="form-check-input"
-                                                type="checkbox"
-                                                role="switch"
-                                                id="flexSwitchCheckChecked"
+                                                className="form-control"
+                                                type="number"
+                                                id="quesLimit"
                                             />
+                                            <p className="form-check-label m-0">
+                                                of 2 questions
+                                            </p>
                                         </div>
                                     </div>
-                                    <div class="ojn81eb">
-                                        <h5
-                                            class="qh8uqkb"
-                                            style={{ marginRight: '.5rem' }}
-                                        >
-                                            <strong>Written</strong>
-                                        </h5>
-                                        <div class="form-check form-switch">
+                                    <div className="quizOptionBlock">
+                                        <legend>NOTE</legend>
+                                        <div className="mb-2">
                                             <input
-                                                class="form-check-input"
+                                                className={`form-check-input ${FormStyles.formCheckInput} ms-0`}
                                                 type="checkbox"
-                                                role="switch"
-                                                id="flexSwitchCheckChecked"
+                                                value=""
+                                                id="note"
                                             />
+                                            <label
+                                                className="form-check-label"
+                                                for="note"
+                                            >
+                                                Show note
+                                            </label>
                                         </div>
                                     </div>
                                 </div>
-                            </form>
-                        </div>
-                        <div class="modal-footer">
-                            <button
-                                type="button"
-                                class="btn btn-secondary"
-                                data-bs-dismiss="modal"
-                            >
-                                Close
-                            </button>
-                            <button type="button" class="btn btn-primary">
-                                Start Quiz
-                            </button>
+                                <div className="col-6">
+                                    <div className="quizOptionBlock mb-4">
+                                        <legend>PROMPT WITH</legend>
+                                        <div className="mb-2">
+                                            <input
+                                                className={`form-check-input ${FormStyles.formCheckInput} ms-0`}
+                                                type="checkbox"
+                                                value=""
+                                                id="note"
+                                            />
+                                            <label
+                                                className="form-check-label"
+                                                for="note"
+                                            >
+                                                Show note
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div className="quizOptionBlock">
+                                        <legend>ANSWER WITH</legend>
+                                        <div className="mb-2">
+                                            <input
+                                                className={`form-check-input ${FormStyles.formCheckInput} ms-0`}
+                                                type="checkbox"
+                                                value=""
+                                                id="note"
+                                            />
+                                            <label
+                                                className="form-check-label"
+                                                for="note"
+                                            >
+                                                Show note
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
