bf00f3316c482d6629689eeda4f6e3abd22e4a13
tuyethg2001 <tuyethg2792001@gmail.com>
2023-07-21 18:07:41 +0700

    FE fix create post

--- react-frontend/src/pages/settings/Notifications.jsx
+++ react-frontend/src/pages/settings/Notifications.jsx
@@ -1,8 +1,103 @@
 const Notifications = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault()
+    }
+
     return (
-        <div className="mx-5 ps-3 pe-5">
-            <h4>Notification Settings</h4>
-            <form></form>
+        <div className="mx-5 ps-3">
+            <h4>Email Notification</h4>
+            <form className="mt-4" noValidate>
+                <fieldset className="form-check">
+                    <legend>STUDY REMINDERS</legend>
+                    <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id="studyReminder"
+                    />
+                    <label className="form-check-label" for="studyReminder">
+                        Email reminders
+                    </label>
+                    <div class="mb-3">
+                        <label
+                            for="exampleFormControlInput1"
+                            class="form-label"
+                        >
+                            Time of day:
+                        </label>
+                        <input
+                            type="time"
+                            class="form-control"
+                            id="exampleFormControlInput1"
+                            placeholder="name@example.com"
+                        />
+                    </div>
+                </fieldset>
+                <fieldset className="form-check">
+                    <legend>IN YOUR CLASS, RECEIVE EMAIL WHEN:</legend>
+                    <div>
+                        <input
+                            className="form-check-input"
+                            type="checkbox"
+                            value=""
+                            id="studyReminder"
+                        />
+                        <label className="form-check-label" for="studyReminder">
+                            A set is added
+                        </label>
+                    </div>
+                    <div>
+                        <input
+                            className="form-check-input"
+                            type="checkbox"
+                            value=""
+                            id="studyReminder"
+                        />
+                        <label className="form-check-label" for="studyReminder">
+                            A post is posted
+                        </label>
+                    </div>
+                    <div>
+                        <input
+                            className="form-check-input"
+                            type="checkbox"
+                            value=""
+                            id="studyReminder"
+                        />
+                        <label className="form-check-label" for="studyReminder">
+                            An assignment is assigned
+                        </label>
+                    </div>
+                    <div>
+                        <input
+                            className="form-check-input"
+                            type="checkbox"
+                            value=""
+                            id="studyReminder"
+                        />
+                        <label className="form-check-label" for="studyReminder">
+                            A test is assigned
+                        </label>
+                    </div>
+                    <div>
+                        <input
+                            className="form-check-input"
+                            type="checkbox"
+                            value=""
+                            id="studyReminder"
+                        />
+                        <label className="form-check-label" for="studyReminder">
+                            Your submission is graded
+                        </label>
+                    </div>
+                </fieldset>
+                <button
+                    className="btn btn-primary px-4 mt-4"
+                    onClick={handleSubmit}
+                >
+                    Save
+                </button>
+            </form>
         </div>
     )
 }
