2395efc9e0e605778b667489cbb0efbd5ee680e9
tuyethg2001 <tuyethg2792001@gmail.com>
2023-07-21 21:54:10 +0700

    FE notification settings

--- react-frontend/src/pages/settings/notification/Notifications.jsx
+++ react-frontend/src/pages/settings/notification/Notifications.jsx
@@ -7,93 +7,149 @@ const Notifications = () => {
     }
 
     return (
-        <div className="mx-5 ps-3">
+        <div className="mx-5 ps-5">
             <h4>Email Notification</h4>
             <form className="mt-4" noValidate>
-                <fieldset className="notification_label form-check mb-4">
-                    <legend>STUDY REMINDERS</legend>
-                    <input
-                        className={`form-check-input ${FormStyles.formCheckInput}`}
-                        type="checkbox"
-                        value=""
-                        id="studyReminder"
-                    />
-                    <label className="form-check-label" for="studyReminder">
-                        Email reminders
-                    </label>
-                    <div class="mb-3">
-                        <label
-                            for="exampleFormControlInput1"
-                            class="form-label"
-                        >
-                            Time of day:
-                        </label>
-                        <input
-                            type="time"
-                            class="form-control"
-                            id="exampleFormControlInput1"
-                            placeholder="name@example.com"
-                        />
+                <div className="row">
+                    <div className="col-6">
+                        <fieldset className="notification_formContainer form-check mb-3 ps-0">
+                            <legend>STUDY REMINDERS</legend>
+                            <input
+                                className={`form-check-input ${FormStyles.formCheckInput} ms-0`}
+                                type="checkbox"
+                                value=""
+                                id="studyReminder"
+                            />
+                            <label
+                                className="form-check-label"
+                                for="studyReminder"
+                            >
+                                Email reminders
+                            </label>
+                            <div class="mt-2 notification_formTime">
+                                <label
+                                    for="studyReminderTime"
+                                    class="form-label"
+                                >
+                                    Time of day:
+                                </label>
+                                <input
+                                    type="time"
+                                    class={`form-control ${FormStyles.formControl} ms-0`}
+                                    id="studyReminderTime"
+                                />
+                            </div>
+                        </fieldset>
                     </div>
-                </fieldset>
-                <fieldset className="form-check">
-                    <legend>IN YOUR CLASS, RECEIVE EMAIL WHEN:</legend>
-                    <div>
-                        <input
-                            className="form-check-input"
-                            type="checkbox"
-                            value=""
-                            id="studyReminder"
-                        />
-                        <label className="form-check-label" for="studyReminder">
-                            A set is added
-                        </label>
+                    <div className="col-6">
+                        <fieldset className="notification_formContainer form-check mb-3 ps-0">
+                            <legend>DUE DATE REMINDERS</legend>
+                            <div>
+                                <input
+                                    className={`form-check-input ${FormStyles.formCheckInput} ms-0`}
+                                    type="checkbox"
+                                    value=""
+                                    id="assignmentReminder"
+                                />
+                                <label
+                                    className="form-check-label"
+                                    for="assignmentReminder"
+                                >
+                                    Assignment reminders
+                                </label>
+                            </div>
+                            <div>
+                                <input
+                                    className={`form-check-input ${FormStyles.formCheckInput} ms-0`}
+                                    type="checkbox"
+                                    value=""
+                                    id="testReminder"
+                                />
+                                <label
+                                    className="form-check-label"
+                                    for="testReminder"
+                                >
+                                    Test reminders
+                                </label>
+                            </div>
+                        </fieldset>
                     </div>
-                    <div>
-                        <input
-                            className="form-check-input"
-                            type="checkbox"
-                            value=""
-                            id="studyReminder"
-                        />
-                        <label className="form-check-label" for="studyReminder">
-                            A post is posted
-                        </label>
+                    <div className="col-12">
+                        <fieldset className="form-check notification_formContainer mb-3 ps-0">
+                            <legend>IN YOUR CLASS, RECEIVE EMAIL WHEN:</legend>
+                            <div>
+                                <input
+                                    className={`form-check-input ${FormStyles.formCheckInput} ms-0`}
+                                    type="checkbox"
+                                    value=""
+                                    id="setAdded"
+                                />
+                                <label
+                                    className="form-check-label"
+                                    for="setAdded"
+                                >
+                                    A set is added
+                                </label>
+                            </div>
+                            <div>
+                                <input
+                                    className={`form-check-input ${FormStyles.formCheckInput} ms-0`}
+                                    type="checkbox"
+                                    value=""
+                                    id="postPosted"
+                                />
+                                <label
+                                    className="form-check-label"
+                                    for="postPosted"
+                                >
+                                    A post is posted
+                                </label>
+                            </div>
+                            <div>
+                                <input
+                                    className={`form-check-input ${FormStyles.formCheckInput} ms-0`}
+                                    type="checkbox"
+                                    value=""
+                                    id="assignmentAssigned"
+                                />
+                                <label
+                                    className="form-check-label"
+                                    for="assignmentAssigned"
+                                >
+                                    An assignment is assigned
+                                </label>
+                            </div>
+                            <div>
+                                <input
+                                    className={`form-check-input ${FormStyles.formCheckInput} ms-0`}
+                                    type="checkbox"
+                                    value=""
+                                    id="testAssigned"
+                                />
+                                <label
+                                    className="form-check-label"
+                                    for="testAssigned"
+                                >
+                                    A test is assigned
+                                </label>
+                            </div>
+                            <div>
+                                <input
+                                    className={`form-check-input ${FormStyles.formCheckInput} ms-0`}
+                                    type="checkbox"
+                                    value=""
+                                    id="submissionGraded"
+                                />
+                                <label
+                                    className="form-check-label"
+                                    for="submissionGraded"
+                                >
+                                    Your submission is graded
+                                </label>
+                            </div>
+                        </fieldset>
                     </div>
-                    <div>
-                        <input
-                            className="form-check-input"
-                            type="checkbox"
-                            value=""
-                            id="studyReminder"
-                        />
-                        <label className="form-check-label" for="studyReminder">
-                            An assignment is assigned
-                        </label>
-                    </div>
-                    <div>
-                        <input
-                            className="form-check-input"
-                            type="checkbox"
-                            value=""
-                            id="studyReminder"
-                        />
-                        <label className="form-check-label" for="studyReminder">
-                            A test is assigned
-                        </label>
-                    </div>
-                    <div>
-                        <input
-                            className="form-check-input"
-                            type="checkbox"
-                            value=""
-                            id="studyReminder"
-                        />
-                        <label className="form-check-label" for="studyReminder">
-                            Your submission is graded
-                        </label>
-                    </div>
-                </fieldset>
+                </div>
                 <button
                     className="btn btn-primary px-4 mt-4"
                     onClick={handleSubmit}
