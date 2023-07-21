7ec6bcddefa2c5ae6c9e5688af1d535d517f2c0e
Quantruong355 <97685567+Quantruong355@users.noreply.github.com>
2023-07-22 00:25:28 +0700

    UT for Models

--- springboot-backend/src/test/java/com/capstone/project/model/AssignmentTest.java
+++ springboot-backend/src/test/java/com/capstone/project/model/AssignmentTest.java
@@ -12,12 +12,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class AssignmentTest {
     SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-    @ParameterizedTest(name = "index => userId={0}, classId={1},created_date{2},instruction{3},due_date{4},modified_date{5},start_date{6} ,title{7}")
+    @ParameterizedTest(name = "index => userId={0}, classId={1},created_date{2},instruction{3},due_date{4},modified_date{5},start_date{6} ,title{7}, is_Draft{8}")
     @CsvSource({
-            "1,3,2023-8-9, Luyen thi JLPT N5,2023-7-1,2023-8-7,2023-8-8, On thi N3 ",
-            "2,4,2023-8-9, Luyen thi JLPT N4,2023-9-9,2023-8-7,2023-8-8, On thi N3 "
+            "1,3,2023-8-9, Luyen thi JLPT N5,2023-8-12,2023-9-9,2023-8-10, On thi N3,true ",
+            "2,4,2023-8-9, Luyen thi JLPT N4,2023-8-13,2023-9-9,2023-8-11, On thi N3,false "
     })
-    public void testAssignment(int userId,int classId, String created_date,String instruction,String due_date,String modified_date,String start_date,String title){
+    public void testAssignment(int userId,int classId, String created_date,String instruction,String due_date,String modified_date,String start_date,String title,boolean isDraft){
              try{
                  Assignment assignment = Assignment.builder()
                          .user(User.builder().id(userId).build())
@@ -28,6 +28,7 @@ public class AssignmentTest {
                          .modified_date(dateFormat.parse(modified_date))
                          .start_date(dateFormat.parse(start_date))
                          .title(title)
+                         .is_draft(isDraft)
                          .build();
 
                  assertThat(assignment).isNotNull();
@@ -39,6 +40,7 @@ public class AssignmentTest {
                  assertThat(assignment.getModified_date()).isEqualTo(modified_date);
                  assertThat(assignment.getStart_date()).isEqualTo(start_date);
                  assertThat(assignment.getTitle()).isEqualTo(title);
+                 assertThat(assignment.is_draft()).isEqualTo(isDraft);
 
              } catch (ParseException e) {
                  throw new RuntimeException(e);
