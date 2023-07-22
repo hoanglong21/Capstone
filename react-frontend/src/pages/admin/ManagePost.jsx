e81a28048a828ba811011e8a79799bd478e8e0c4
Đỗ Thùy Dương <thuyduong03112001@gmail.com>
2023-07-23 01:53:20 +0700

    add post

--- react-frontend/src/pages/admin/ManagePost.jsx
+++ react-frontend/src/pages/admin/ManagePost.jsx
@@ -69,7 +69,7 @@ function ManagePost() {
                     <td>{posts?.created_date}</td>
                     <td>
                       <Link
-                        to={`/viewdetailpost/${posts.id}`}
+                        to={`/viewdetailpost/${posts.classroom?.id}`}
                         className="btn btn-primary me-3"
                       >
                         <i class="bi bi-info-square me-2"></i>
