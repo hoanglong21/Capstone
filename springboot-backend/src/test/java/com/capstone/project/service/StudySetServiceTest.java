dc200bffe625db78aa8951644d2249d015e5d9a1
hoanglong21 <longdh21@gmail.com>
2023-07-23 17:28:14 +0700

    UT StudySet and StudySetType

--- springboot-backend/src/test/java/com/capstone/project/service/StudySetServiceTest.java
+++ springboot-backend/src/test/java/com/capstone/project/service/StudySetServiceTest.java
@@ -2,10 +2,7 @@ package com.capstone.project.service;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import static org.mockito.ArgumentMatchers.any;
 
@@ -18,6 +15,8 @@ import com.capstone.project.repository.StudySetRepository;
 import com.capstone.project.repository.UserRepository;
 import com.capstone.project.service.impl.StudySetServiceImpl;
 import com.capstone.project.service.impl.UserServiceImpl;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
@@ -44,6 +43,15 @@ public class StudySetServiceTest {
     @Mock
     private ContentRepository contentRepository;
 
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private CardService cardService;
+
+    @Mock
+    private EntityManager entityManager;
+
     @InjectMocks
     private StudySetServiceImpl studySetServiceImpl;
 
@@ -56,7 +64,7 @@ public class StudySetServiceTest {
 
     @Order(1)
     @Test
-    void testGetAllStudySets() {
+    public void testGetAllStudySets() {
         StudySet studySet = StudySet.builder()
                 .title("Stub")
                 .studySetType(StudySetType.builder().id(1).build())
@@ -75,7 +83,7 @@ public class StudySetServiceTest {
             "1, title1, description1, true, false, false, 1, 2023-6-7",
             "2, title2, description2, false, true, false, 2, 2023-6-8",
     })
-    void testCreateStudySet(int userId, String title, String description, boolean is_deleted, boolean is_public,
+    public void testCreateStudySet(int userId, String title, String description, boolean is_deleted, boolean is_public,
                         boolean is_draft, int studySetTypeId, String deleted_date) {
         try {
             StudySet studySet = StudySet.builder()
@@ -105,7 +113,7 @@ public class StudySetServiceTest {
             "1, title1, description1, true, false, true, 1, 2023-6-7",
             "2, title2, description2, false, true, false, 2, 2023-6-8"
     })
-    void testUpdateStudySet(int userId, String title, String description, boolean is_deleted, boolean is_public,
+    public void testUpdateStudySet(int userId, String title, String description, boolean is_deleted, boolean is_public,
                             boolean is_draft, int studySetTypeId, String deleted_date) {
         try {
             StudySet studySet = StudySet.builder()
@@ -142,7 +150,7 @@ public class StudySetServiceTest {
 
     @Order(4)
     @Test
-    void deleteStudySet() {
+    public void deleteStudySet() {
         StudySet studySet = StudySet.builder()
                 .title("Stub")
                 .is_deleted(false)
@@ -160,7 +168,7 @@ public class StudySetServiceTest {
 
     @Order(5)
     @Test
-    void deleteHardStudySet() {
+    public void deleteHardStudySet() {
         StudySet studySet = StudySet.builder()
                 .id(1)
                 .title("Stub")
@@ -189,6 +197,92 @@ public class StudySetServiceTest {
         verify(contentRepository, times(1)).delete(content);
     }
 
+    @Order(6)
+    @Test
+    public void checkBlankCard() {
+        StudySet studySet = StudySet.builder()
+                .id(1)
+                .title("Stub")
+                .studySetType(StudySetType.builder().id(1).build())
+                .build();
+        Card card = Card.builder().id(1).studySet(studySet).build();
+
+        when(studySetRepository.findById(anyInt())).thenReturn(Optional.of(studySet));
+        when(cardRepository.getCardByStudySetId(anyInt())).thenReturn(List.of(card));
+        try {
+            when(cardService.checkBlank(anyInt())).thenReturn(true);
+        } catch (ResourceNotFroundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            List<Integer> list = studySetServiceImpl.checkBlankCard(1);
+            assertThat(list.size()).isGreaterThan(0);
+        } catch (ResourceNotFroundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Order(7)
+    @Test
+    public void getAllStudySetByUser() {
+        User user = User.builder().username("stubUser").build();
+        StudySet studySet = StudySet.builder()
+                .id(1)
+                .title("Stub")
+                .studySetType(StudySetType.builder().id(1).build())
+                .build();
+
+
+        when(userRepository.findUserByUsername(anyString())).thenReturn(user);
+        when(studySetRepository.findStudySetByAuthor_id(anyInt())).thenReturn(List.of(studySet));
+
+        try {
+            List<StudySet> list = studySetServiceImpl.getAllStudySetByUser("stubUser");
+            assertThat(list.size()).isGreaterThan(0);
+        } catch (ResourceNotFroundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Order(8)
+    @ParameterizedTest(name = "{index} => isDeleted={0}, isPublic={1}, isDraft={2}, search={3}, type={4}, authorId={5}, authorName={6}, " +
+            "fromDeleted={7}, toDeleted={8}, fromCreated={9}, toCreated={10}, sortBy={11}, direction={12}, page={13}, size={14}")
+    @CsvSource({
+            "true, false, null, , 1, 0, , , , , , id, ASC, 1, 5",
+            "false, true, false, test, 2, 0, , , , , , title, DESC, 1, 5"
+    })
+    public void testGetFilterList(Boolean isDeleted, Boolean isPublic, Boolean isDraft, String search, int type, int authorId, String authorName,
+                           String fromDeleted, String toDeleted, String fromCreated, String toCreated,
+                           String sortBy, String direction, int page, int size) {
+        MockitoAnnotations.openMocks(this);
+        StudySet studySet = StudySet.builder()
+                .id(1)
+                .title("Stub")
+                .studySetType(StudySetType.builder().id(1).build())
+                .build();
+        Query mockedQuery = mock(Query.class);
+        when(entityManager.createNativeQuery(anyString(), eq("StudySetResponseCustomListMapping"))).thenReturn(mockedQuery);
+        when(mockedQuery.setParameter(anyString(), any())).thenReturn(mockedQuery);
+        when(mockedQuery.getResultList()).thenReturn(List.of(studySet));
+
+        List<StudySet> list = (List<StudySet>) studySetServiceImpl.getFilterList(isDeleted, isPublic, isDraft, search, type, authorId, authorName,
+                fromDeleted, toDeleted, fromCreated, toCreated, sortBy, direction, page, size).get("list");
+
+        assertThat(list.size()).isGreaterThan(0);
+    }
+
+    @Order(9)
+    @Test
+    public void getQuizByStudySetId() {
+        when(cardService.getAllByStudySetId(anyInt())).thenReturn(new ArrayList<>());
+        when(contentRepository.getContentByCardId(anyInt())).thenReturn(new ArrayList<>());
+
+
+        List<Map<String, Object>> response = studySetServiceImpl.getQuizByStudySetId(1, "multiple choice", 1);
+        assertThat(response.size()).isEqualTo(0);
+
+    }
 
 
 }
