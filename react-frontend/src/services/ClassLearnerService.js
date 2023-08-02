8ee5c791bbd92af0cf36987b5345d5ffce00f1b3
tuyethg2001 <tuyethg2792001@gmail.com>
2023-08-02 16:35:51 +0700

    FE request to join class

--- react-frontend/src/services/ClassLearnerService.js
+++ react-frontend/src/services/ClassLearnerService.js
@@ -10,12 +10,14 @@ const createClassLeaner = (classLearner) => {
     })
 }
 
-const deleteClassLearner = (classLearner) => {
-    return axios.delete(API_BASE_URL + '/classlearner/', classLearner, {
-        headers: {
-            Authorization: `Bearer ${localStorage.getItem('userToken')}`,
-        },
-    })
+const deleteClassLearner = (userid, classid) => {
+    return axios.delete(
+        API_BASE_URL +
+            '/delclasslearner?userid=' +
+            userid +
+            '&classid=' +
+            classid
+    )
 }
 
 const filterGetLeaner = (
