f50183e06ed6abbb464403e0cc55f37bac6c4749
hoanglong21 <longdh21@gmail.com>
2023-08-06 05:51:36 +0700

    Update for deploy

--- react-frontend/src/services/AuthService.js
+++ react-frontend/src/services/AuthService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:8080/api/v1/auth'
+const API_BASE_URL = process.env.REACT_APP_HOST + '/auth'
 
 const registration = (user) => {
     return axios.post(API_BASE_URL + '/register', user)
