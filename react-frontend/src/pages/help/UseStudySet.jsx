696d63ddefa46fa6534464bbfb62b3b0ab444f94
Đỗ Thùy Dương <thuyduong03112001@gmail.com>
2023-08-05 17:02:32 +0700

    add

--- react-frontend/src/pages/help/UseStudySet.jsx
+++ react-frontend/src/pages/help/UseStudySet.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
+import React from "react";
 
 function UseStudySet() {
   return (
     <div>
-        <div className="container">
-            <div>
-                <h4>Study Flashcard?</h4>
-            </div>
+      <div className="container">
+        <div>
+          <h4>Study Flashcard?</h4>
+          <ul>
+            <li>
+              User can select multiple types of study in flashcards to display.
+            </li>
+            <li>
+              After that, press the <strong>"Create new flashcards"</strong>{" "}
+              button to get started.
+            </li>
+          </ul>
         </div>
+        <div>
+          <h4>Do Quiz?</h4>
+          <ul>
+            <li>User can select multiple types of quiz to display.</li>
+            <li>
+              After that, press the <strong>"Create new quiz"</strong> button to
+              get started.
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h4>Learn?</h4>
+          <ul>
+            <li>User can select multiple types of quiz to display.</li>
+            <li>
+              After that, press the <strong>"Start"</strong> button to get
+              started.
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default UseStudySet
+export default UseStudySet;
