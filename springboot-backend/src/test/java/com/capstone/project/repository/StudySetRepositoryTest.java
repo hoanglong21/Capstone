37b27ac4ec2ee3dfa40aed389ad375d7d2fb4060
hoanglong21 <longdh21@gmail.com>
2023-08-18 19:09:02 +0700

    Finish Study Set Test

--- springboot-backend/src/test/java/com/capstone/project/repository/StudySetRepositoryTest.java
+++ springboot-backend/src/test/java/com/capstone/project/repository/StudySetRepositoryTest.java
@@ -1,7 +1,9 @@
 package com.capstone.project.repository;
 import com.capstone.project.model.*;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -9,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -47,5 +51,47 @@ public class StudySetRepositoryTest {
             List<StudySet> result = studySetRepository.findStudySetByAuthor_id(-1);
             assertThat(result.size()).isEqualTo(0);
         }
+    }
+
+    @Order(2)
+    @Test
+    public void countCardInSetWithCondition() {
+        User userInit = User.builder()
+                .username("test")
+                .email("test99999@gmail.com")
+                .build();
+        userRepository.save(userInit);
+
+        StudySet studySetInit = StudySet.builder()
+                .studySetType(StudySetType.builder().id(1).build())
+                .user(userInit)
+                .build();
+
+        studySetRepository.save(studySetInit);
+
+        Integer count= studySetRepository.countCardInSetWithCondition(studySetInit.getId(), userInit.getId(), "not study", false);
+        Assertions.assertThat(count).isEqualTo(0);
+    }
+
+    @Order(3)
+    @Test
+    public void findListIdToDelete() {
+        Date currentDate = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(currentDate);
+        calendar.add(Calendar.DAY_OF_MONTH, -31);
+        Date date30DaysAgo = calendar.getTime();
+
+        StudySet studySetInit = StudySet.builder()
+                .id(99999)
+                .studySetType(StudySetType.builder().id(1).build())
+                .user(User.builder().id(1).build())
+                .is_deleted(true)
+                .deleted_date(date30DaysAgo)
+                .build();
+        studySetRepository.save(studySetInit);
+
+        List<Integer> deleteInt = studySetRepository.findListIdToDelete();
+        Assertions.assertThat(deleteInt.size()).isGreaterThan(0);
     }
 }
