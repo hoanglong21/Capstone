1b0e266dfeaf690bd073a47d6e1359d612a75293
Đỗ Thùy Dương <thuyduong03112001@gmail.com>
2023-08-20 00:18:31 +0700

    update

--- react-frontend/src/pages/admin/ReplyFeedback.jsx
+++ react-frontend/src/pages/admin/ReplyFeedback.jsx
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from "react";
-import FormStyles from '../../assets/styles/Form.module.css'
+import FormStyles from "../../assets/styles/Form.module.css";
 import FeedbackService from "../../services/FeedbackService";
 import SidebarforAdmin from "./SidebarforAdmin";
 import HeaderAdmin from "./HeaderAdmin";
 import { Link, useParams } from "react-router-dom";
 
 function ReplyFeedback() {
-  const [feedback, setFeedback] = useState([])
+  const [feedback, setFeedback] = useState([]);
   const [error, setError] = useState("");
   const [feedbackR, setFeedbackR] = useState([]);
-  const [success, setSuccess] = useState(false)
-  const {id} = useParams();
-
+  const [success, setSuccess] = useState(false);
+  const { id } = useParams();
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
       const temp = (await FeedbackService.getFeedbackById(id)).data;
@@ -20,9 +20,9 @@ function ReplyFeedback() {
     const fetchDataR = async () => {
       setFeedbackR({
         id: feedback.id,
-        title: '',
-        content: '',
-    })
+        title: "",
+        content: "",
+      });
     };
     if (id) {
       fetchData();
@@ -32,17 +32,23 @@ function ReplyFeedback() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-      try {
-        await FeedbackService.replyfeedback(feedback.id, feedbackR.title, feedbackR.content);
-        setError("");
-        setSuccess(true)
-      } catch (error) {
-        if (error.response && error.response.data) {
-          setError(error.response.data);
-        } else {
-          setError(error.message);
-        }
+    setLoading(true);
+    try {
+      await FeedbackService.replyfeedback(
+        feedback.id,
+        feedbackR.title,
+        feedbackR.content
+      );
+      setError("");
+      setSuccess(true);
+    } catch (error) {
+      if (error.response && error.response.data) {
+        setError(error.response.data);
+      } else {
+        setError(error.message);
       }
+    }
+    setLoading(false);
   };
 
   return (
@@ -52,73 +58,84 @@ function ReplyFeedback() {
         <div className="col-sm">
           <HeaderAdmin />
           <div className="container">
-          <h3 className="text-center">Reply Feedback</h3>
-          <div className="mx-5">
-             {/* error message */}
-             {error && (
-                    <div
-                        className="error alert alert-danger"
-                        role="alert"
-                        dangerouslySetInnerHTML={{ __html: error }}
-                    ></div>
+            <h3 className="text-center">Reply Feedback</h3>
+            <div className="mx-5">
+              {/* error message */}
+              {error && (
+                <div
+                  className="error alert alert-danger"
+                  role="alert"
+                  dangerouslySetInnerHTML={{ __html: error }}
+                ></div>
+              )}
+              {/* success */}
+              {success && (
+                <div className="alert alert-success" role="alert">
+                  Send reply successfully!
+                </div>
+              )}
+              <input
+                type="text"
+                className={`form-control ${FormStyles.formControl}`}
+                id="id"
+                value={feedback.id}
+                hidden
+              />
+              <p className="mb-2 text-info fs-5">Title</p>
+              <input
+                type="text"
+                className={`form-control ${FormStyles.formControl}`}
+                id="title"
+                placeholder="Title"
+                value={feedbackR.title}
+                onChange={(event) => {
+                  setFeedbackR({
+                    title: event.target.value,
+                  });
+                }}
+                required
+              />
+              <p className="ms-2 mb-2 text-info fs-5">Content</p>
+              <textarea
+                id="content"
+                className={`form-control ${FormStyles.formControl}`}
+                placeholder="Message"
+                style={{ height: "6rem" }}
+                value={feedbackR.content}
+                onChange={(event) => {
+                  setFeedbackR({
+                    content: event.target.value,
+                  });
+                }}
+              ></textarea>
+            </div>
+            <div className="text-center mt-4">
+              <Link
+                type="button"
+                className="btn btn-secondary me-4"
+                to={`/viewdetailfb/${feedback.id}`}
+              >
+                Back
+              </Link>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={handleSubmit}
+                disabled={loading}
+              >
+                {loading ? (
+                  <div
+                    className="spinner-border text-secondary mx-auto mb-1"
+                    role="status"
+                    id="loading"
+                  >
+                    <span className="visually-hidden">Loading...</span>
+                  </div>
+                ) : (
+                  "Send"
                 )}
-                {/* success */}
-                {success && (
-                    <div className="alert alert-success" role="alert">
-                       Send reply successfully!
-                    </div>
-                )}
-          <input
-            type="text"
-            className={`form-control ${FormStyles.formControl}`}
-            id="id"
-            value={feedback.id}
-            hidden
-          />
-          <p className="mb-2 text-info fs-5">Title</p>
-          <input
-            type="text"
-            className={`form-control ${FormStyles.formControl}`}
-            id="title"
-            placeholder="Title"
-            value={feedbackR.title}
-            onChange={(event) => {
-              setFeedbackR({
-                title: event.target.value,
-              });
-            }}
-            required
-          />
-          <p className="ms-2 mb-2 text-info fs-5">Content</p>
-          <textarea
-            id="content"
-            className={`form-control ${FormStyles.formControl}`}
-            placeholder="Message"
-            style={{ height: "6rem" }}
-            value={feedbackR.content}
-            onChange={(event) => {
-              setFeedbackR({
-                content: event.target.value,
-              });
-            }}
-          ></textarea>
-          </div>
-          <div className="text-center mt-4">
-          <Link
-            type="button"
-            className="btn btn-secondary me-4"
-            to={`/viewdetailfb/${feedback.id}`}
-          >
-            Back
-          </Link>
-          <button
-            type="button"
-            className="btn btn-primary"
-            onClick={handleSubmit}
-          >
-            Send
-          </button>
-          </div>
+              </button>
+            </div>
           </div>
         </div>
       </div>
