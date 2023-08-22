5cb1ff565bc55ffc3026324fc5dfdb59f297951a
Đỗ Thùy Dương <thuyduong03112001@gmail.com>
2023-08-23 01:13:27 +0700

    add

--- react-frontend/src/components/footer/Footer.jsx
+++ react-frontend/src/components/footer/Footer.jsx
@@ -47,10 +47,10 @@ const Footer = () => {
                 <div className="footer border-top p-3 px-4 d-flex justify-content-between">
                     <div className="d-flex column-gap-3">
                         <a className="footer-link" href="privacy">
-                            Privacy
+                        {t('privacy')}
                         </a>
                         <a className="footer-link" href="term">
-                            Terms
+                        {t('terms')}
                         </a>
                     </div>
                     <div>
@@ -72,67 +72,67 @@ const Footer = () => {
                 <div className="siteFooter">
                     <div className="siteFooter-top d-flex row">
                         <div className="d-flex flex-column col">
-                            <h5>About us</h5>
+                            <h5> {t('aboutUs')}</h5>
                             <a className="siteFooter-sectionLink" href="#">
-                                About Nihongo Level Up
+                            {t('about')} Nihongo Level Up      
                             </a>
                             <a className="siteFooter-sectionLink" href="#">
-                                Careers
+                            {t('careers')}
                             </a>
                             <a className="siteFooter-sectionLink" href="#">
-                                Advertise with us
-                            </a>
-                        </div>
-                        <div className="d-flex flex-column col">
-                            <h5>For Learners</h5>
-                            <a className="siteFooter-sectionLink" href="#">
-                                Flashcards
-                            </a>
-                            <a className="siteFooter-sectionLink" href="#">
-                                Learn
-                            </a>
-                            <a className="siteFooter-sectionLink" href="#">
-                                Dictionary
-                            </a>
-                            <a className="siteFooter-sectionLink" href="#">
-                                Translate
+                            {t('advertiseWithUs')}
                             </a>
                         </div>
                         <div className="d-flex flex-column col">
-                            <h5>For Tutors</h5>
+                            <h5> {t('forLearners')}</h5>
                             <a className="siteFooter-sectionLink" href="#">
-                                Classes
+                            {t('flashcard')}
                             </a>
                             <a className="siteFooter-sectionLink" href="#">
-                                Checkpoint
-                            </a>
-                        </div>
-                        <div className="d-flex flex-column col">
-                            <h5>Resources</h5>
-                            <a className="siteFooter-sectionLink" href="#">
-                                Help centre
+                            {t('learn')}
                             </a>
                             <a className="siteFooter-sectionLink" href="#">
-                                Sign up
+                            {t('dictionary')}
                             </a>
                             <a className="siteFooter-sectionLink" href="#">
-                                Honour Code
-                            </a>
-                            <a className="siteFooter-sectionLink" href="#">
-                                Community Guidelines
-                            </a>
-                            <a className="siteFooter-sectionLink" href="#">
-                                Privacy
-                            </a>
-                            <a className="siteFooter-sectionLink" href="#">
-                                Terms
-                            </a>
-                            <a className="siteFooter-sectionLink" href="#">
-                                Ad and Cookie Policy
+                            {t('translate')}
                             </a>
                         </div>
                         <div className="d-flex flex-column col">
-                            <h5>Language</h5>
+                            <h5> {t('forTutor')}</h5>
+                            <a className="siteFooter-sectionLink" href="#">
+                            {t('class')}
+                            </a>
+                            <a className="siteFooter-sectionLink" href="#">
+                            {t('checkpoint')}
+                            </a>
+                        </div>
+                        <div className="d-flex flex-column col">
+                            <h5> {t('resources')}</h5>
+                            <a className="siteFooter-sectionLink" href="#">
+                            {t('helpcenter')}
+                            </a>
+                            <a className="siteFooter-sectionLink" href="#">
+                            {t('signup')}
+                            </a>
+                            <a className="siteFooter-sectionLink" href="#">
+                            {t('honourCode')}
+                            </a>
+                            <a className="siteFooter-sectionLink" href="#">
+                            {t('communityGuidelines')}
+                            </a>
+                            <a className="siteFooter-sectionLink" href="#">
+                            {t('privacy')}
+                            </a>
+                            <a className="siteFooter-sectionLink" href="#">
+                            {t('terms')}
+                            </a>
+                            <a className="siteFooter-sectionLink" href="#">
+                            {t('adandCookiePolicy')}
+                            </a>
+                        </div>
+                        <div className="d-flex flex-column col">
+                            <h5> {t('language')}</h5>
                             <select className="siteFooter-select" name="" id="">
                                 <option value="">English</option>
                                 <option value="">Tiếng Việt</option>
