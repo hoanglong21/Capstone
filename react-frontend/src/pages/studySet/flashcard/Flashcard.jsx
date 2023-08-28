56e00aa4054b439a1b9808dc816a2728542211e2
tuyethg2001 <tuyethg2792001@gmail.com>
2023-08-28 21:46:09 +0700

    fix

--- react-frontend/src/pages/studySet/flashcard/Flashcard.jsx
+++ react-frontend/src/pages/studySet/flashcard/Flashcard.jsx
@@ -559,9 +559,19 @@ const Flashcard = () => {
         tempCard.studySet.user.created_date = toBEDate(
             tempCard.studySet.user.created_date
         )
+        tempCard.studySet.user.dob = toBEDate(tempCard.studySet.user.dob)
+        tempCard.studySet.user.deleted_date = toBEDate(
+            tempCard.studySet.user.deleted_date
+        )
+        tempCard.studySet.user.banned_date = toBEDate(
+            tempCard.studySet.user.banned_date
+        )
         var tempUser = {
             ...userInfo,
-            created_date: toBEDate(userInfo.created_date),
+            created_date: toBEDate(userInfo.dob),
+            dob: toBEDate(userInfo.dob),
+            deleted_date: toBEDate(userInfo.deleted_date),
+            banned_date: toBEDate(userInfo.banned_date),
         }
         var tempProgress = {
             user: tempUser,
